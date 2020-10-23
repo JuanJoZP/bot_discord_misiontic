3623f229cbb0775c0cf968549bf0b2dc64d6d795
Juan José Zuluaga Patiño <juanzpjose136@gmail.com>
2020-10-23 09:36:51 -0500

    Update bot.js
    Documentación de autorol

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 
-const role_id = "750551197328605294";  //ENTUSIASTA
+const role_id = "750551197328605294";  //ROL ENTUSIASTA
 
 //Consola
 client.once("ready", async msg =>{
@@ -13,7 +13,7 @@ client.on("guildMemberAdd", async member => {
     const autoRole = member.guild.roles.cache.get(role_id);
     
     if (!autoRole) return;
-
+    //ASIGNA AUTOMATICAMENTE EL ROL DE ENTUSIASTA
     member.role.add(autoRole.id)
 });
 
