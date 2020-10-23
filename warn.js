8bc281d3f73c1c33820b32f34f88d5180cf93733
Juan José Zuluaga Patiño <juanzpjose136@gmail.com>
2020-10-23 09:35:04 -0500

    Update warn.js
    Limpieza y documentación de el código de sistema de advertencias

--- warn.js
+++ warn.js
@@ -1,19 +1,14 @@
-/*
-UTILIZA EL BUSCADOR DE CARACTERES Y PON: variab3ls_
-
-CON ESE PALABRA DE REMITIRA A TODAS LAS VARIABLES QUE DEBES CAMBIAR POR LAS DE TU SERVIDOR
-*/
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs")
-const prefix = config.prefix;
+const prefix = config.prefix; //CPARA CAMBIAR EL PREFIX DE LOS COMANDOS ENTRAR EN ARCHIVO CONFIG.JSON
 const usuarios = require("./warns.json");
 
-const admin = "756000768317849692"; //ROL 1 DE ADMIN - variab3ls_
-const mod = "768270744299503686";// ROL 2 DE ADMIN - variab3ls_
-const canal_warns = "768266552478662657" //CANAL DONDE NOTIFICA LOS WARNS - variab3ls_
+const admin = "756000768317849692"; //CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
+const mod = "768270744299503686";//SEGUNDO ROL DE ADMIN - CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
+const canal_warns = "768266552478662657" //CAMBIAR POR ID DE EL CANAL DONDE SE MOSTRARAN LOS WARNS
+var canal = "768657967678619649" //CAMBIAR POR EL CANAL DE BANEOS
 
 //Consola
 client.once("ready", async msg =>{
@@ -51,11 +46,9 @@ function registrarWarns (user) {
 //APLICA EL WARNEO A EL USUARIO EN CUESTION, ACTUALIZA LA BASE DE DATOS, Y ENVIA EL MENSAJE AVISANDO QUE EL USUARIO FUE WARNEADO
 function warneado (user, member, reason) {
     var warn = usuarios[user.id].warns;
-    var limit = 2;
-    var respuesta;
-    var canal = "768657967678619649" //CAMBIAR POR EL CANAL DE BANEOS  - variab3ls_
+    var limit = 2; //CAMBIAR POR LA CANTIDAD DE WARNS A RECIBIR ANTES DE SER BANEADO - 1
     
-    //cuando el usuario acumula 3 advertencias es baneado permanentemente
+    //cuando el usuario acumula (limit + 1) advertencias es baneado permanentemente
     if (warn == limit) {
         if (member) {
             member
@@ -66,9 +59,7 @@ function warneado (user, member, reason) {
                 client.channels.cache.get(canal).send(`El usuario ${user} ha sido baneado.`);
             })
             .catch(err => {
-                if (member.hasPermission("ADMINISTRATOR")) {
-                    
-                }
+                console.log(err);
             });
             usuarios[user.id].warns = 0;
             fs.writeFile("./warns.json", JSON.stringify(usuarios), (err) => {
@@ -76,18 +67,19 @@ function warneado (user, member, reason) {
             });
         
         }
-    //cuando lleva menos de 3 warneos, se muestra el mensaje de warneo y se actualiza la base de datos
+    //cuando lleva menos de (limit + 1) warneos, se muestra el mensaje de warneo y se actualiza la base de datos
+    //CAMBIAR EMBED PARA QUE CONTENGA LOS DATOS RESPECTIVOS A CADA SERVIDOR
     } else {
         usuarios[user.id].warns = warn + 1;
         //|||||||||||||||EMBED
         const embed = new Discord.MessageEmbed()
         .setColor('#FF0000')
-        .setTitle('WARNS | MisionTic2 020')
+        .setTitle('WARNS | MisionTic2020') 
         .setAuthor(client.user.username, 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
         .setDescription(`<@${user.id}> ** | Ha recibido un warn ${warn + 1} debido a ${reason}. **`)
         .setThumbnail("https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png")
         .setTimestamp()
-        .setFooter('©MinTic | 2020 | Esteban - Juanjo', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
+        .setFooter('©MinTic | 2020 | Esteban - JuanJo', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
         //|||||||||||||||EMBED
         fs.writeFile("./warns.json", JSON.stringify(usuarios), (err) => {
             if(err) console.log(err);
@@ -111,7 +103,7 @@ client.on('message', msg => {
 //MENSAJE PARA WARNEAR
 client.on("message", async message => {
     if (message.content.startsWith(`${prefix}warn`)) {
-        if (message.member.roles.cache.get(admin) || message.member.roles.cache.get(mod)) { //AQUI SE AGREGAN MAS ROLES DE ADMINISTRADORES - variab3ls_
+        if (message.member.roles.cache.get(admin) || message.member.roles.cache.get(mod) //AQUI SE AGREGAN MAS ROLES DE ADMINISTRADORES) { 
             var user = message.mentions.users.first();
             var reason = message.content.split(' ')[2]
             var member = message.guild.member(user);
