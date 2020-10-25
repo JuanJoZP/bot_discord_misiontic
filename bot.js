0096ef4f533e3f9902d22d8bffff1c3054dc2f8f
JuanJoZP <juanzpjose136@gmail.com>
2020-10-25 16:23:01 -0500

    warns añadido al entry point

--- bot.js
+++ bot.js
@@ -3,8 +3,11 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const prefix = config.prefix;
 const levels = require('./levels.js')
-
+const warn = require('./warn.js')
 const role_id = "750551197328605294";  //ROL ENTUSIASTA
+const admin = "756000768317849692"; //CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
+const mod = "768270744299503686";//SEGUNDO ROL DE ADMIN - CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
+
 
 //Consola
 client.once("ready", () => {
@@ -28,6 +31,7 @@ client.on("guildMemberAdd", async member => {
 client.on("message", async message => {
     if(message.author.bot) return;
 
+
     if(levels.usuarioRegistrado(message.author)) {
         const verNivel = new levels.verNivel(message.author);
 
@@ -46,8 +50,53 @@ client.on("message", async message => {
 });
 
 
-//---------------------WARNS-----------------
+//---------------------WARNS---------------------
 
+//COMPRUEBA SI EL USUARIO ESTA REGISTRADO EN LA DB DE WARNS
+client.on("message", message => {
+    if (warn.usuariosWarneados(message.author)) return;
+    else warn.registrarWarns(message.author);
+})
+
+//MENSAJE PARA WARNEAR
+client.on("message", async message => {
+    if (message.content.startsWith(`${prefix}warn`)) {
+
+        //AQUI SE AGREGAN MAS ROLES DE ADMINISTRADORES)
+        if (message.member.roles.cache.get(admin) || message.member.roles.cache.get(mod)) { 
+            var user = message.mentions.users.first();
+            var reason = message.content.split(' ')[2]
+            var member = message.guild.member(user);
+
+            message.delete();
+            if (!member.hasPermission("ADMINISTRATOR")) {
+
+                const warneo =  new warn.warneado(user, member, reason);
+
+                try {
+                    console.log(warneo.response)
+                    if (warneo.response) {
+                        message.channel.send(warneo.response)
+                    } else {
+                        throw 'ban'
+                    }
+                } catch (err) {
+                    if (err == 'ban') {
+                        message.channel.send(`El usuario ${user} ha sido baneado`)
+                    }
+                    console.log(err)
+                }
+
+            } else {
+                message.reply(`No puede warnear a ${user}.`);
+            }
+
+        } else {
+            message.delete();
+            message.reply('No tienes permisos para realizar esta acción');
+        }
+    }
+});
 
 //token
 client.login(config.token);
--- warn.js
+++ warn.js
@@ -1,19 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
 const fs = require("fs")
-const prefix = config.prefix; //CPARA CAMBIAR EL PREFIX DE LOS COMANDOS ENTRAR EN ARCHIVO CONFIG.JSON
 const usuarios = require("./warns.json");
-
-const admin = "756000768317849692"; //CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
-const mod = "768270744299503686";//SEGUNDO ROL DE ADMIN - CAMBIAR POR LA ID DE EL ROL DE ADMIN EN CADA SERVIDOR
-const canal_warns = "768266552478662657" //CAMBIAR POR ID DE EL CANAL DONDE SE MOSTRARAN LOS WARNS
-var canal = "768657967678619649" //CAMBIAR POR EL CANAL DE BANEOS
-
-//Consola
-client.once("ready", async msg =>{
-    console.log('Warns Iniciado')
-});
 
 //COMPRUEBA SI EL USUARIO ESTA REGISTRADO EN LA DB
 function usuariosWarneados (user) {
@@ -56,7 +44,7 @@ function warneado (user, member, reason) {
                 reason: reason
             })
             .then(() => {
-                client.channels.cache.get(canal).send(`El usuario ${user} ha sido baneado.`);
+                console.log('EL usuario fue baneado')
             })
             .catch(err => {
                 console.log(err);
@@ -75,7 +63,7 @@ function warneado (user, member, reason) {
         const embed = new Discord.MessageEmbed()
         .setColor('#FF0000')
         .setTitle('WARNS | MisionTic2020') 
-        .setAuthor(client.user.username, 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
+        .setAuthor('MinTic 2022', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
         .setDescription(`<@${user.id}> ** | Ha recibido un warn ${warn + 1} debido a ${reason}. **`)
         .setThumbnail("https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png")
         .setTimestamp()
@@ -85,42 +73,12 @@ function warneado (user, member, reason) {
             if(err) console.log(err);
         });
         
-        client.channels.cache.get(canal_warns).send(embed);
+        this.response = embed;
     }
 }
 
-//REVISAR SI EL USUARIO ESTA REGISTRADO EN LA BASE DE DATOS
-client.on('message', msg => {
-    if (msg.author.bot) return;
-
-    //llamada a funcion de registrar en la base de datos
-    if (usuariosWarneados(msg.author)) return;
-    else registrarWarns(msg.author);
-});
 
 
-
-//MENSAJE PARA WARNEAR
-client.on("message", async message => {
-    if (message.content.startsWith(`${prefix}warn`)) {
-        if (message.member.roles.cache.get(admin) || message.member.roles.cache.get(mod) //AQUI SE AGREGAN MAS ROLES DE ADMINISTRADORES) { 
-            var user = message.mentions.users.first();
-            var reason = message.content.split(' ')[2]
-            var member = message.guild.member(user);
-
-            message.delete();
-            if (!member.hasPermission("ADMINISTRATOR")) {
-                warneado(user, member, reason);
-            } else {
-                message.reply(`No puede warnear a ${user}.`);
-            }
-
-        } else {
-            message.delete();
-            message.reply('No tienes permisos para realizar esta acción');
-        }
-    }
-});
-
-//TOKEN
-client.login(config.token);
+module.exports.usuariosWarneados = usuariosWarneados;
+module.exports.registrarWarns = registrarWarns;
+module.exports.warneado = warneado;
