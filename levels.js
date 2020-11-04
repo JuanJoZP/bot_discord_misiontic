998336b5195bdd41513b0f0e7707d93af50f868c
Juan José Zuluaga Patiño <juanzpjose136@gmail.com>
2020-11-04 14:49:03 -0500

    funcionalidad: DB creadas automaticamente
    las bases de datos warns.json y users_levels.json son creadas
    automaticamente en caso de no existir, y por ultimo son requeridas,
    permitiendo eliminar los archivos del repositorio.

--- levels.js
+++ levels.js
@@ -1,89 +1,115 @@
-const fs = require("fs")
-const usuarios = require("./users_levels.json");
+const fs = require('fs');
+var usuarios;
+if (fs.existsSync('./users_levels.json')) {
+	usuarios = require('./users_levels.json');
+} else {
+	fs.writeFile('./users_levels.json', '{}', (err) => {
+		if (err) {
+			console.log(err);
+		}
+		usuarios = require('./users_levels.json');
+	});
+}
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
- //REGISTRAR USUARIOS
-function usuarioRegistrado (user) {
-    try{
-        if(!usuarios[user.id]) return false;
-        else return true;
-    } catch (e) {
-        return false;
-    }
+//REGISTRAR USUARIOS
+function usuarioRegistrado(user) {
+	try {
+		if (!usuarios[user.id]) return false;
+		else return true;
+	} catch (e) {
+		return false;
+	}
 }
 
-function registrarUsuario (user) {
-    try{
-        usuarios[user.id] = {
-            nivel: 1,
-            xp: 0,
-        }
-        console.log(`Datos creados para ${user.tag}`);
+function registrarUsuario(user) {
+	try {
+		usuarios[user.id] = {
+			nivel: 1,
+			xp: 0,
+		};
+		console.log(`Datos creados para ${user.tag}`);
 
-        fs.writeFile("./users_levels.json", JSON.stringify(usuarios), (err) => {
-            if(err) console.log(err);
-        });
-
-    } catch (e) {
-        return;
-    }
+		fs.writeFile('./users_levels.json', JSON.stringify(usuarios), (err) => {
+			if (err) console.log(err);
+		});
+	} catch (e) {
+		return;
+	}
 }
 
 //PEDIR NIVEL Y XP
-function verNivel (user) {
-    var nivel = usuarios[user.id].nivel;
-    var xp = usuarios[user.id].xp;
-    var sigNivel = nivel * 100;
-    var agregarXp = 10;
-    if (xp>= sigNivel) {
-        usuarios[user.id].xp = 0;
-        usuarios[user.id].nivel = nivel + 1;
-        fs.writeFile("./users_levels.json", JSON.stringify(usuarios), (err) => {
-            if(err) console.log(err);
-        });
-        //Embed inicio
-        this.embed = new Discord.MessageEmbed()
-        .setColor('#2336A2')
-        .setTitle('NIVELES | MISION TIC 2022')
-        .setAuthor('MinTic 2022', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-        .setDescription(`El usuario <@${user.id}>  ha ascendido al nivel ${nivel + 1}`)
-        .setThumbnail("https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png")
-        .setTimestamp()
-        .setFooter('©MinTic | 2020 | Esteban - Juanjo', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-        
-
-
-    } else  {
-        usuarios[user.id].xp = xp + agregarXp;
-        this.embed = new Discord.MessageEmbed()
-        fs.writeFile("./users_levels.json", JSON.stringify(usuarios), (err) => {
-            if(err) console.log(err);
-        });
-    }
-    
+function verNivel(user) {
+	var nivel = usuarios[user.id].nivel;
+	var xp = usuarios[user.id].xp;
+	var sigNivel = nivel * 100;
+	var agregarXp = 10;
+	if (xp >= sigNivel) {
+		usuarios[user.id].xp = 0;
+		usuarios[user.id].nivel = nivel + 1;
+		fs.writeFile('./users_levels.json', JSON.stringify(usuarios), (err) => {
+			if (err) console.log(err);
+		});
+		//Embed inicio
+		this.embed = new Discord.MessageEmbed()
+			.setColor('#2336A2')
+			.setTitle('NIVELES | MISION TIC 2022')
+			.setAuthor(
+				'MinTic 2022',
+				'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+			)
+			.setDescription(
+				`El usuario <@${user.id}>  ha ascendido al nivel ${nivel + 1}`
+			)
+			.setThumbnail(
+				'https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png'
+			)
+			.setTimestamp()
+			.setFooter(
+				'©MinTic | 2020 | Esteban - Juanjo',
+				'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+			);
+	} else {
+		usuarios[user.id].xp = xp + agregarXp;
+		this.embed = new Discord.MessageEmbed();
+		fs.writeFile('./users_levels.json', JSON.stringify(usuarios), (err) => {
+			if (err) console.log(err);
+		});
+	}
 }
 
 function consultarXp(message) {
-    this.user = message.author;
-    this.nivel = usuarios[this.user.id].nivel;
-    this.xp = usuarios[this.user.id].xp;
-    this.sigNivel = this.nivel * 100;
+	this.user = message.author;
+	this.nivel = usuarios[this.user.id].nivel;
+	this.xp = usuarios[this.user.id].xp;
+	this.sigNivel = this.nivel * 100;
 
-    this.embed = new Discord.MessageEmbed()
-    .setColor('#EB5B77')
-    .setTitle('NIVELES | MISION TIC 2022')
-    .setAuthor('MinTic 2022', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-    .addFields(
-        {name: `** Aca podras consultar tu nivel. **`, value: `${this.user} | TU NIVEL ES:` },
-        {name: 'Nivel: ', value: `${this.nivel}` },
-        {name: 'XP: ', value: `${this.xp}/${this.sigNivel}`}
-    )
-    .setThumbnail('https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png')
-    .setTimestamp()
-    .setFooter('©MinTic | 2020 | Esteban - Juanjo', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-    //|||||||||||||||EMBED
-
+	this.embed = new Discord.MessageEmbed()
+		.setColor('#EB5B77')
+		.setTitle('NIVELES | MISION TIC 2022')
+		.setAuthor(
+			'MinTic 2022',
+			'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+		)
+		.addFields(
+			{
+				name: `** Aca podras consultar tu nivel. **`,
+				value: `${this.user} | TU NIVEL ES:`,
+			},
+			{ name: 'Nivel: ', value: `${this.nivel}` },
+			{ name: 'XP: ', value: `${this.xp}/${this.sigNivel}` }
+		)
+		.setThumbnail(
+			'https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png'
+		)
+		.setTimestamp()
+		.setFooter(
+			'©MinTic | 2020 | Esteban - Juanjo',
+			'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+		);
+	//|||||||||||||||EMBED
 }
 
 module.exports.usuarioRegistrado = usuarioRegistrado;
--- warn.js
+++ warn.js
@@ -1,83 +1,100 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs")
-const usuarios = require("./warns.json");
+const fs = require('fs');
+if (fs.existsSync('./warns.json')) {
+	const usuarios = require('./warns.json');
+} else {
+	fs.writeFile('./warns.json', '{}', (err) => {
+		if (err) {
+			console.log(err);
+		}
+		const usuarios = require('./warns.json');
+	});
+}
 
 //COMPRUEBA SI EL USUARIO ESTA REGISTRADO EN LA DB
-function usuariosWarneados (user) {
-    try{
-        if(!usuarios[user.id]) return false;
-        else return true;
-    } catch (e) {
-        return false;
-    }
+function usuariosWarneados(user) {
+	try {
+		if (!usuarios[user.id]) return false;
+		else return true;
+	} catch (e) {
+		return false;
+	}
 }
 
 //REGISTRAR USUARIOS EN LA BASE DE DATOS
-function registrarWarns (user) {
-    try{
-        usuarios[user.id] = {
-            warns: 0,
-        }
+function registrarWarns(user) {
+	try {
+		usuarios[user.id] = {
+			warns: 0,
+		};
 
-        console.log(`${user.tag} fué registrado`);
+		console.log(`${user.tag} fué registrado`);
 
-        fs.writeFile("./warns.json", JSON.stringify(usuarios), (err) => {
-            if(err) console.log(err);
-        });
-
-    } catch (e) {
-        return;
-    }
+		fs.writeFile('./warns.json', JSON.stringify(usuarios), (err) => {
+			if (err) console.log(err);
+		});
+	} catch (e) {
+		return;
+	}
 }
 
 //APLICA EL WARNEO A EL USUARIO EN CUESTION, ACTUALIZA LA BASE DE DATOS, Y ENVIA EL MENSAJE AVISANDO QUE EL USUARIO FUE WARNEADO
-function warneado (user, member, reason) {
-    var warn = usuarios[user.id].warns;
-    var limit = 2; //CAMBIAR POR LA CANTIDAD DE WARNS A RECIBIR ANTES DE SER BANEADO - 1
-    
-    //cuando el usuario acumula (limit + 1) advertencias es baneado permanentemente
-    if (warn == limit) {
-        if (member) {
-            member
-            .ban({
-                reason: reason
-            })
-            .then(() => {
-                console.log('EL usuario fue baneado')
-            })
-            .catch(err => {
-                console.log(err);
-            });
-            usuarios[user.id].warns = 0;
-            fs.writeFile("./warns.json", JSON.stringify(usuarios), (err) => {
-                   if(err) console.log(err);
-            });
-        
-        }
-    //cuando lleva menos de (limit + 1) warneos, se muestra el mensaje de warneo y se actualiza la base de datos
-    //CAMBIAR EMBED PARA QUE CONTENGA LOS DATOS RESPECTIVOS A CADA SERVIDOR
-    } else {
-        usuarios[user.id].warns = warn + 1;
-        //|||||||||||||||EMBED
-        const embed = new Discord.MessageEmbed()
-        .setColor('#FF0000')
-        .setTitle('WARNS | MisionTic2020') 
-        .setAuthor('MinTic 2022', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-        .setDescription(`<@${user.id}> ** | Ha recibido un warn ${warn + 1} debido a ${reason}. **`)
-        .setThumbnail("https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png")
-        .setTimestamp()
-        .setFooter('©MinTic | 2020 | Esteban - JuanJo', 'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png')
-        //|||||||||||||||EMBED
-        fs.writeFile("./warns.json", JSON.stringify(usuarios), (err) => {
-            if(err) console.log(err);
-        });
-        
-        this.response = embed;
-    }
+function warneado(user, member, reason) {
+	var warn = usuarios[user.id].warns;
+	var limit = 2; //CAMBIAR POR LA CANTIDAD DE WARNS A RECIBIR ANTES DE SER BANEADO - 1
+
+	//cuando el usuario acumula (limit + 1) advertencias es baneado permanentemente
+	if (warn == limit) {
+		if (member) {
+			member
+				.ban({
+					reason: reason,
+				})
+				.then(() => {
+					console.log('EL usuario fue baneado');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+			usuarios[user.id].warns = 0;
+			fs.writeFile('./warns.json', JSON.stringify(usuarios), (err) => {
+				if (err) console.log(err);
+			});
+		}
+		//cuando lleva menos de (limit + 1) warneos, se muestra el mensaje de warneo y se actualiza la base de datos
+		//CAMBIAR EMBED PARA QUE CONTENGA LOS DATOS RESPECTIVOS A CADA SERVIDOR
+	} else {
+		usuarios[user.id].warns = warn + 1;
+		//|||||||||||||||EMBED
+		const embed = new Discord.MessageEmbed()
+			.setColor('#FF0000')
+			.setTitle('WARNS | MisionTic2020')
+			.setAuthor(
+				'MinTic 2022',
+				'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+			)
+			.setDescription(
+				`<@${user.id}> ** | Ha recibido un warn ${
+					warn + 1
+				} debido a ${reason}. **`
+			)
+			.setThumbnail(
+				'https://cdn.discordapp.com/attachments/755973908456538146/766351219886522388/articles-150235_logo.png'
+			)
+			.setTimestamp()
+			.setFooter(
+				'©MinTic | 2020 | Esteban - JuanJo',
+				'https://cdn.discordapp.com/attachments/755973908456538146/766365052541468672/misiontic.png'
+			);
+		//|||||||||||||||EMBED
+		fs.writeFile('./warns.json', JSON.stringify(usuarios), (err) => {
+			if (err) console.log(err);
+		});
+
+		this.response = embed;
+	}
 }
-
-
 
 module.exports.usuariosWarneados = usuariosWarneados;
 module.exports.registrarWarns = registrarWarns;
